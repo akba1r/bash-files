2f27295e78d2af99c8df8c6feac6ad68b1b64468
akba1r <129611049+akba1r@users.noreply.github.com>
2023-04-01 21:53:05 +0600

    Create sh.sh

---
+++ sh.sh
@@ -0,0 +1,7 @@
+sudo cp apktool /usr/local/bin
+sudo cp apktool.jar /usr/local/bin
+./msfinstall
+sudo apt install zipalign -y
+sudo apt install apksigner -y
+sudo apt install apktool -y
+./ngrok tcp 5555
